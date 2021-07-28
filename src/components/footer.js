9901b390a3701c15da8c8741b3f0cf05a35c9553
ArildStark <87885663+ArildStark@users.noreply.github.com>
2021-07-28 16:51:19 +0800

    Add files via upload

---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+import React from "react"
+const SmallFeet = () =>  {
+
+return(
+
+
+<footer>
+<p> created by Arild Stark </p>
+ </footer>
+     )
+
+}
+
+export default SmallFeet
---
+++ src/components/header.js
@@ -0,0 +1,45 @@
+import React from "react"
+import { Link } from "gatsby"
+import  '../MarkStyle/markStyle.css'
+
+
+
+const NavHeader = () =>  {
+
+return(
+
+
+<div class="divnavbar">
+<br></br>
+<br></br>
+<br></br>
+
+<body>
+<br></br>
+<br></br>
+    <header> 
+    <nav>
+    <br></br>
+<br></br>
+    <ul>   
+
+  
+    <li><a href ="/" class="NavbarLink" >Home</a></li>
+    <li><a href ="/about/" class="NavbarLink" >About</a></li>
+    <li><a href="/blogs/" class="NavbarLink">Blog</a></li>
+    <li><a href="/contact/" class="NavbarLink" >Contact</a></li>
+    
+    </ul> 
+   
+    </nav>
+
+    </header>
+    </body>
+    </div>
+
+  
+)
+
+}
+
+export default NavHeader
